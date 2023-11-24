d53062966b588d8baefcbad7d0bab0faad3cb607
NEERAJ023 <143694023+NEERAJ023@users.noreply.github.com>
2023-11-24 22:19:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your code here
+document.addEventListener('DOMContentLoaded', function () {
+const images = document.querySelectorAll('.image');
+
+  let draggedImage = null;
+
+  images.forEach(image => {
+    image.addEventListener('dragstart', handleDragStart);
+    image.addEventListener('dragover', handleDragOver);
+    image.addEventListener('drop', handleDrop);
+  });
+
+  function handleDragStart(event) {
+    draggedImage = event.target;
+     // necessary for Firefox
+  }
+
+  function handleDragOver(event) {
+    event.preventDefault();
+  }
+
+  function handleDrop(event) {
+    const target = event.target;
+
+    // Ensure the drop target is a valid image element
+    if (target.classList.contains('image')) {
+      // Swap background images
+      const temp = target.style.backgroundImage;
+      target.style.backgroundImage = draggedImage.style.backgroundImage;
+      draggedImage.style.backgroundImage = temp;
+    }
+
+    draggedImage = null;
+  }
+});
